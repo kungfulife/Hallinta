51c9dd2defb494d91ca17adb40ab50859f0a499a
Fedor Alexandrov <falexandrov@outlook.com>
2025-06-07 12:56:44 -0400

    - Added base code/Ui of user bottom cmd explaination - Reset to default now works - Ui Update Changes - Added Steam Icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,21 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::command;
+use std::path::PathBuf;
+use dirs::home_dir;
+
+#[command]
+fn get_noita_config_path() -> String {
+    if let Some(profile_dir) = home_dir() {
+        let local_low = profile_dir.join("AppData").join("LocalLow");
+        let noita_path = local_low.join("Nolla_Games_Noita").join("save00").join("mod_config.xml");
+        noita_path.to_string_lossy().into_owned()
+    } else {
+        "C:\\Users\\Default\\AppData\\LocalLow\\Nolla_Games_Noita\\save00\\mod_config.xml".to_string()
+    }
+}
 
 fn main() {
-    halinta_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_noita_config_path])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
